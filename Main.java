6716cd6a7c464a1a53b627f4089404fdcb957b49
kamil-kayan <55934910+kamil-kayan@users.noreply.github.com>
2021-03-15 02:33:33 +0300

    Core Java - Inheritance
    Inheritance, A simple program depicting the re-usability in Inheritance in Java.(Core Java)

--- Main.java
+++ Main.java
@@ -1,14 +1,86 @@
+package com.company;
+/** Inheritance Allows us to Create new Classes from the Existing Once.
+ * It Allows Re-usability of already created code*/
+class Student{
+    private int id;
+    private String name;
+    private String address;
+    private int marks;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
+
+class Teacher extends Student{
+    private int id;
+    private float salary;
+
+
+    public int getId(){
+        return this.id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
-        Dimensions dimensions = new Dimensions(20, 20, 5);
-        Case theCase = new Case("220", "Hp", 230, dimensions);
+        Teacher teacher = new Teacher();
+        Student st = new Student();
+        teacher.setName("Osman");
+        teacher.setSalary(30000);
+        teacher.setAddress("Bula Dhagax, Garissa");
+        teacher.setId(23);
+        st.setName("Oyow");
+        st.setMarks(358);
+        st.setId(6023);
 
-        Monitor theMonitor = new Monitor("27inch Beast", "Dell", 29, new Resolution(23, 22));
-        Motherboard motherboard = new Motherboard("Bj-200", "Acer", 4, 6, "v2.23");
-
-        PC thePC = new PC(motherboard, theMonitor, theCase);
-        thePC.powerUp();
-        thePC.progLoader();//calling progLoader form PC class'
+        System.out.println("Student's Name: "+st.getName()+"\nStudent's id: "+st.getId()+
+                "\nStudent's Marks: "+st.getMarks());
+        System.out.println("Teacher's name: "+teacher.getName()+"\nTeacher's id:"+teacher.getId()+
+                "\nTeacher's address: "+teacher.getAddress()+"\nTeacher's Salary: "+teacher.getSalary());
 
 
     }
